a74ac1749ae1f280e4e814e14d95e927efb39a14
Roman Zhbadynskyi <zhbadynskyi@gmail.com>
2022-06-20 04:00:28 +0300

    cleanup

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for entry in ./assets/*.png; do
-  echo "${entry##*/}"
-  echo "${entry%.png}"
-  echo $(basename "${entry}" ".${entry##*.}")
-  foo=basename "${entry}" ".${entry##*.}"
-  echo $(foo)
-done
